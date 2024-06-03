225adf571084a0b505d9c8fd79db84b1e9cc9319
Guillermo <guillermo@zeppelinlabs.io>
2024-06-03 19:20:42 -0300

    added prueba

--- src/app/todos.service.ts
+++ src/app/todos.service.ts
@@ -1,9 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Todo } from './types';
 
 @Injectable({
   providedIn: 'root',
 })
+
 export class TodosService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+  }
+
+  getTodos(): Observable<Todo[]>{
+    return this.http.get("https://jsonplaceholder.typicode.com/todos") as Observable<Todo[]>;
+  }
+
+  getById(id:number): Observable<Todo>{
+    return this.http.get(`https://jsonplaceholder.typicode.com/todos/${id}`) as Observable<Todo>;
+  }
 }
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { TodosService } from '../todos.service';
+import { Todo } from '../types';
 
 @Component({
   selector: 'app-todos',
@@ -7,7 +8,35 @@ import { TodosService } from '../todos.service';
   styleUrls: ['./todos.component.scss'],
 })
 export class TodosComponent {
+  todos:Todo[] = [];
+  alltodos:Todo[] = [];
+  userId: string | undefined;
+
   constructor(private todosService: TodosService) {}
 
-  randomMethod() {}
+  ngOnInit() {
+    this.todosService.getTodos().subscribe((data) => {
+      this.todos = data;
+      this.alltodos = data;
+    });
+  }
+
+  filterByUserId() {
+    this.userId = document.querySelector("input")?.value
+
+    if(this.userId){
+      this.todos = this.alltodos;
+      this.todos = this.todos.filter((todo: any) => `${todo.userId}` === this.userId)
+    }else{
+      this.todos = this.alltodos;
+    }
+
+  }
+
+  getDetails(id:number) {
+    this.todosService.getById(id).subscribe((todo) => {
+      alert(`Title: ${todo.title}, Completed: ${todo.completed}, ID: ${todo.id}, User ID: ${todo.userId}`)
+    });
+
+  }
 }
